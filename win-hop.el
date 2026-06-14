6b4c759c0435d13356b060b91cda7a6da6f7b523
Nikita Onachko <behollder.kh@gmail.com>
2026-06-14 10:08:01 +0100

    docs(package): add licence wording

--- win-hop.el
+++ win-hop.el
@@ -7,6 +7,21 @@
 ;; Keywords: convenience, window, location
 ;; Homepage: https://github.com/behollder/win-hop
 
+;; This file is NOT part of GNU Emacs.
+;;
+;; This program is free software: you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation, either version 3 of the
+;; License, or (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Commentary:
 ;; This package overrides the header-line of all visible windows,
 ;; assigns a shortcut letter to each, and jumps to the window.
